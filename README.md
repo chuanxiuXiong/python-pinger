902c398a1a69dd6bd7788eef61e92f591f41b50b
Chuanxiu Xiong (Ethan) <chuanxix@usc.edu>
2018-11-04 17:42:05 -0800

    'implementation-completed#1'

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # python-pinger
+
+## Table of Contents
+- [Introduction](#introduction)
+- [Getting Started](#getting-started)
+- [A Couple Things to Note](#a-couple-things-to-note)
+
+## Introduction
+
+This is a simple python implementation of pinger.
+
+## Getting Started
+
+1. Install [Python2]()
+2. Install all the packages listed in packages.txt. If you have [pip](https://pypi.org/project/pip/) installed, you could run `pip install -r packages.txt` to install all the packages. If for some reason it does not work, please manually install all the libraries initialized at the beginning of pinger.py
+3. To run the program:
+    ```
+        python pinger.py -d destinationIP -c count -l logFile -p payload
+    ```
+
+## A Couple Things To Note:
+1. Since the grading does not include logfile, nothing will be written to the logfile that the user specifies.
+2. Options are optional. Only when there are no options is there going to be an error message. DestinationIP is by default set to '8.8.8.8'. Count is by default set to '1'. Payload is by default set to 'hello'.
+3. The program does not handle invalid options in the arguments of command line, which means count has to be an int and larger than 0, destinationIP has to be a valid IP seperated by dots. As the instruction suggests, the program does not look up the IP address in the DNS server, so the destinationIP cannot be a hostname. The program also assumes we use IPv4.
--- pinger.py
+++ pinger.py
@@ -10,25 +10,24 @@ import os
 ICMP_ECHO_REQUEST = 8
 
 
-def checksum(source_string):
-    temp_sum = 0
-    count_to = (len(source_string) / 2) * 2
+def checksum(str):
+    csum = 0
+    countTo = (len(str) / 2) * 2
     count = 0
-    while count < count_to:
-        this_val = ord(source_string[count + 1])*256+ord(source_string[count])
-        temp_sum = temp_sum + this_val
-        temp_sum = temp_sum & 0xffffffff  # Necessary?
+    while count < countTo:
+        thisVal = ord(str[count+1]) * 256 + ord(str[count])
+        csum = csum + thisVal
+        csum = csum & 0xffffffffL
         count = count + 2
-    if count_to < len(source_string):
-        temp_sum = temp_sum + ord(source_string[len(source_string) - 1])
-        temp_sum = temp_sum & 0xffffffff  # Necessary?
-    temp_sum = (temp_sum >> 16) + (temp_sum & 0xffff)
-    temp_sum = temp_sum + (temp_sum >> 16)
-    answer = ~temp_sum
+    if countTo < len(str):
+        csum = csum + ord(str[len(str) - 1])
+        csum = csum & 0xffffffffL
+    csum = (csum >> 16) + (csum & 0xffff)
+    csum = csum + (csum >> 16)
+    answer = ~csum
     answer = answer & 0xffff
     answer = answer >> 8 | (answer << 8 & 0xff00)
     return answer
-
 
 def ping(dest, timeout, packet_id, sequence_num, payload):
     try:
@@ -38,28 +37,26 @@ def ping(dest, timeout, packet_id, sequence_num, payload):
         print("cannot establish socket")
         return
     sock.setblocking(0)
-    # process_id = os.getpid() & 0xFFFF
     check_sum = 0
     dummy_header = struct.pack(
         'bbHHh', ICMP_ECHO_REQUEST, 0, check_sum, packet_id, sequence_num
     )
-    check_sum = socket.htons(checksum(payload + dummy_header))
+    payload_d = struct.pack('p', payload)
+    check_sum = socket.htons(checksum(dummy_header + payload_d))
     header = struct.pack('bbHHh', ICMP_ECHO_REQUEST, 0,
                          check_sum, packet_id, sequence_num)
+    packet = header + payload_d
+
+    # AF_INET address must be tuple, not str
     start_time = time.time()
-    packet = header + payload
-    print("Pinging {} with {} bytes of data {}".format(
-        dest, len(payload), payload))
     start_time = 0
     while packet:
         try:
             start_time = time.time()
-            size = sock.sendto(header + payload, (dest, 1))
+            size = sock.sendto(packet, (dest, 1))
         except:
             print("Failed to send the packet")
         packet = packet[size:]
-
-    print("Finished socket")
 
     data = ''
     while True:
@@ -67,17 +64,15 @@ def ping(dest, timeout, packet_id, sequence_num, payload):
             return -1
         # non-blocking as it is set as non-blocking previously
         try:
-            data, _ = sock.recvfrom(1024)
+            data, addr = sock.recvfrom(1024)
             break
         except:
             # when it doesn't receive data immediately
-            continue
+            pass
     recv_time = time.time()
-    received_header = data[0:20]
+    (version, c_type, length,id, flags, ttl, protocol, check__sum, src_ip, dest_ip) = struct.unpack('!BBHHHBBHII', data[0:20])
     (res_type, res_code, res_checksum, res_id, res_seq) = struct.unpack(
         'bbHHh', data[20:28])
-    print("res_type {}\n res_code {}\n res_checksum {}\n res_id {}\n res_seq{}".format(
-        res_type, res_code, res_checksum, res_id, res_seq))
     if not(res_type == 0 and res_code == 0):
         print("Error...Packet received with ICMP type %d code %d \n",
               res_type, res_code)
@@ -85,28 +80,26 @@ def ping(dest, timeout, packet_id, sequence_num, payload):
         print(
             "Error...Packet received with packetId %d, supposed to be %d \n", res_id, packet_id)
     else:
-        # res_time = struct.unpack('d', data[28:36])
+        print("Reply from {}: bytes={}, time={}ms, TTL={}").format(dest, len(payload), (recv_time - start_time) * 1000 // 1, ttl)
         return recv_time - start_time
 
 
 def main(argv):
     # variale declarations
-    payload = ''
-    count = 0
-    dest = ''
+    payload = 'hello'
+    count = 1
+    dest = '8.8.8.8'
 
     # get command line options
     try:
-        myopts, args = getopt.getopt(argv, "d:p:c:")
+        myopts, args = getopt.getopt(argv, "d:p:c:l:")
     except getopt.GetoptError as e:
         print(str(e))
-        print("Usage: %s -s serverIP -p portno -l logfile -n name" %
-              sys.argv[0])
+        print("Options not correct. Usage: -d -p -c -l")
         sys.exit(2)
 
     if not myopts:
-        print("Usage: %s -s serverIP -p portno -l logfile -n name" %
-              sys.argv[0])
+        print("Options not correct. Usage: -d -p -c -l")
         sys.exit(2)
 
     for k, v in myopts:
@@ -122,13 +115,25 @@ def main(argv):
 
     rtts = []
     packet_id = np.random.choice(range(count), replace=False)
-    for _ in range(count):
-        rtt = ping(dest, timeout, packet_id, 1, payload)
-        rtts.append(rtt)
-        time.sleep(timeout - rtt if timeout - rtt > 0 else 0)
-        print("printing the rtts")
-        print(rtt)
+    minRtt = 1
+    maxRtt = 0
+    print("Pinging {} with {} bytes of data \"{}\"".format(
+        dest, len(payload), payload))
 
+    for i in range(count):
+        rtt = ping(dest, timeout, packet_id, 1, payload)
+        if (rtt != -1):
+            rtts.append(rtt)
+            if rtt < minRtt:
+                minRtt = rtt
+            if rtt > maxRtt:
+                maxRtt = rtt
+        time.sleep(timeout - rtt if timeout - rtt > 0 else 0)
+    averageRtt = sum(rtts) / len(rtts)
+    print("Ping statistics for {}:".format(dest))
+    print("Packets: Sent={}, Received={}, Lost={}({}% loss)".format(count, len(rtts), count - len(rtts), (count-len(rtts))/count))
+    print("Approximate round trip times in milli-seconds:")
+    print("Minimum = {}ms,  Maximum = {}ms, Average={}ms".format(minRtt * 1000 // 1,maxRtt * 1000 // 1,averageRtt * 1000 // 1))
 
 if __name__ == "__main__":
     main(sys.argv[1:])
